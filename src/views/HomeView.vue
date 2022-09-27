97b20b5e8b6bf24a4cd5c7d31e7b548475887296
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-09-27 16:27:59 +0200

    changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <template>
 <div class="home">
-  
+
 </div>
 </template>
 
 <script>
  
   export default {
-
+    data: ()=>({
+      width:2,
+      radius:10,
+      padding:8,
+      lineCap:'round'
+    })
   }
 </script>
